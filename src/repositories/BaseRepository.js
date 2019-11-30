a0b3b71ec4f5f2b362ab2021d57d5c72c22c22b8
unknown <okocha.chukwutem@gmail.com>
2019-11-30 20:13:38 +0100

    add base repository and admin repository models

---
+++ src/repositories/BaseRepository.js
@@ -0,0 +1,36 @@
+class BaseRepository {
+  constructor(model) {
+    if (model === null || model === undefined) {
+      throw new Error("Repository must havae a model attribute passed");
+    }
+    this.model = model;
+  }
+
+  async findById(id) {
+    return await this.model.findByPk(id);
+  }
+
+  async findAll() {
+    return await this.model.findAll();
+  }
+
+  async create(obj) {
+    return await this.model.create(obj);
+  }
+
+  async update(id, obj) {
+    return await this.model.update(obj, { returning: true, where: { id: id } });
+  }
+
+  async findFirst(condition) {
+    return await this.model.findOne({ where: condition });
+  }
+
+  async find(condition) {
+    return await this.model.findAll({ where: condition });
+  }
+
+  // TODO: add delete method
+}
+
+module.exports = BaseRepository;
